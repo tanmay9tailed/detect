5f4f224046670e84b80fd47afb07ba590b7ec7c9
Tanmay Sahu <sahutanmay1903@gmail.com>
2025-01-22 15:27:14 +0530

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+userAgent = navigator.userAgent
+if(userAgent.includes("Android"))
+    document.getElementById("Main-H1").innerText = "Android"
+else
+    document.getElementById("Main-H1").innerText = "Web APP"
